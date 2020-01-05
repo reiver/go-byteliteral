009704b1378b6c85ac646b415120235e82c663f6
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-05 13:34:38 -0800

    docs

---
+++ doc.go
@@ -0,0 +1,12 @@
+/*
+Package byteliteral provides more literals for Go's byte type; including binary literals.
+
+This is a work around for Go not having built-in binary literals.
+
+Example
+
+Example usage:
+
+	var b byte = byteliteral.B10011111
+*/
+package byteliteral
